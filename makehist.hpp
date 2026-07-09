69bcdd513851f3bd2728d0b57bf28ef039e29192
Sasakura Shunki <shunkisa24812@outlook.jp>
2026-07-10 04:07:20 +0900

    modify jumping error

--- makehist.hpp
+++ makehist.hpp
@@ -13,20 +13,30 @@ class highthist : public TH1S{
 	public:
 		highthist(const char *name,const char *title,Int_t nbinsx,Double_t xlow,Double_t xup): TH1S(name, title, nbinsx, xlow, xup) 
 	{
+		wunit = 1500;
+		binlen = 1;
+		baselen = 100;
+		basepeaklen = 100;
 		xmax = xup;
+		thre = 70;
+		bl = 0;
+		bpeaknum = 0;
 	}
-		int wunit = 1500;
-		int binlen = 1;
-		int baselen = 100;
-		int xmax = 15000;
+		int wunit;
+		int binlen;
+		int baselen;
+		int basepeaklen;
+		int xmax;
+		short thre;
 		void Loadhight(const string);
 		void LoadDecaytime(const string);
 		void Setparam(int, int, int, int);
 		void DrawDecay(const string, TCanvas &);
-		short thre = 70;
 	private:
-		short bl = 0;
+		short bl;
+		short bpeaknum;
 		short get_baseline(const string , int);
+		short get_peaknum(const string , int);
 	protected:
 		using TH1S::TH1S;
 };
