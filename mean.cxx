c7ec60a54bda7f6f72b751b82eb76294390ce5f8
Sasakura Shunki <shunkisa24812@outlook.jp>
2026-07-16 19:46:56 +0900

    decayhist use datafile.txt to read and add Print in mean.cxx.

--- mean.cxx
+++ mean.cxx
@@ -1,10 +1,18 @@
 void mean(){
-	auto file = new TFile("decay_hist.root");
-	TH1S *th1 = (TH1S*)file -> Get("th1");
 	// TF1 *f1 = new TF1("decay","[2] * exp(- (x + [1])  / [0]) + [3]",200 ,4600);
 	TF1 *f1 = new TF1("decay","[2] * exp(- (x + [1])  / [0]) + [3]",400 ,4400);
+
+	auto file = new TFile("decay_hist.root");
+	auto c = new TCanvas("c","c");
+	TH1S *th1 = (TH1S*)file -> Get("th1");
 	f1 ->SetParameters(1000, 0, 50, 0);
 	f1 ->FixParameter(1, 0);
 	f1 ->FixParameter(3, 0);
 	th1->Fit(f1,"R");
+	// gStyle->SetOptStat(1000000001);
+	gStyle->SetOptStat(0);
+	gStyle->SetOptFit(1);
+	c->Update();
+
+	c->Print("fitgraph.jpg");
 }
